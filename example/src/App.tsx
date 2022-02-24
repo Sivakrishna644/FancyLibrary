2efc9b091646d70742c585455690063905769881
Siva Krishna Singasani <sivakrishna.singasani@atmecs.com>
2022-02-24 10:00:14 +0530

    First Commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'fancy';
+import { multiply, add, substraction } from 'fancy';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
+  const [result1, setResult1] = React.useState<number | undefined>();
+  const [result2, setResult2] = React.useState<number | undefined>();
 
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
+    add(3, 7).then(setResult1);
+    substraction(3, 7).then(setResult2);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Multiply Result: {result}</Text>
+      <Text>Add Result: {result1}</Text>
+      <Text>Minus Result: {result2}</Text>
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+export function add(a: number, b: number): Promise<number> {
+  return Promise.resolve(a + b);
+}
+export function substraction(a: number, b: number): Promise<number> {
+  return Promise.resolve(a - b);
+}
